ce4878855e48735361e6fe63201391438bc90618
Robiul Islam Rubel <59814051+rubel-uu@users.noreply.github.com>
2021-03-06 16:29:47 +0600

    Add files via upload

---
+++ Toph_NAC_SAC.cpp
@@ -0,0 +1,82 @@
+/*                  Robiul Islam Rubel
+        Depertment of Computer Science & Engineering
+                    Uttara University
+                   Dhaka,Bangladesh
+ */
+
+#include<bits/stdc++.h>
+#define ll                long long int
+#define ld                long double
+#define pb                push_back
+#define pf                push_front
+#define ppb               pop_back
+#define ppf               pop_front
+#define sz                size()
+#define pi                acos(-1.0)
+#define mod               1000000007
+#define F                 first
+#define S                 second
+#define C                 continue
+#define min3(a,b,c)       min(a,min(b,c))
+#define max3(a,b,c)       max(a,max(b,c))
+#define min4(a,b,c,d)     min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)     max(a,max(b,max(c,d)))
+#define mem(a,b)          memset(a,b,sizeof(a))
+#define all(a)            (a).begin(),(a).end()
+#define gcd(a,b)          __gcd(a,b)
+#define lcm(a,b)          ((a)*((b)/gcd(a,b)))
+#define case(a)           cout<<"Case "<<(a)<<": "
+#define point(a)          fixed<<setprecision(a)
+#define get(a)            for(auto &x:a) cin>>x
+#define print(a)          for(auto x:a) cout<<x<<" "; cout<<endl
+#define see(x)            cout<<#x<<" = "<<x<<endl
+#define uniq(a)           sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
+#define big(a)            transform((a).begin(),(a).end(),(a).begin(),::toupper)
+#define small(a)          transform((a).begin(),(a).end(),(a).begin(),::tolower)
+#define ischar(a)         (('a'<=a&&a<='z')('A'<=a&&a<='Z'))
+#define isvowel(a)        (a=='a'|a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+#define fast              ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define tc        int test; cin>>test; while(test--)
+using namespace std;
+int main()
+{
+  unsigned ll n,a,b,c,odd,even,sum1,sum2,t;
+   cin>>t;
+   while(t--)
+   {
+
+     string s;
+     cin>>n;
+     cin>>s;
+    /// ll l=s.size();
+     sum1=sum2=1;
+     for(int i=0;i<n;i++)
+     {
+         if(s[i]=='0')
+         {
+             sum1=sum1*2;
+             if(sum1>100003)
+             {
+                 sum1=sum1%100003;
+             }
+             ///cout<<"For zeor << "<<sum1<<endl;
+         }
+         else
+         {
+             sum2=sum2*3;
+             if(sum2>100003)
+             {
+                 sum2=sum2%100003;
+             }
+             ///cout<<"For one : "<<sum2<<endl;
+         }
+     }
+     cout<<(sum1+sum2)%100003<<endl;
+   }
+
+
+
+
+
+       return 0;
+}
